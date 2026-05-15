41cba83db0fd0bd5e607d7ddcd8cfb8a3052e52a
sebastian.cantergiani <sebastiancantergiani@gmail.com>
2026-05-15 14:32:20 -0400

    feat: nvim transparent bg

---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,12 @@
+return {
+  {
+    "folke/tokyonight.nvim",
+    opts = {
+      transparent = true, -- Enable transparency
+      styles = {
+        sidebars = "transparent", -- Transparent sidebars
+        floats = "transparent", -- Transparent floating windows
+      },
+    },
+  },
+}
